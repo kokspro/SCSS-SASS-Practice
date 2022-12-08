bfabdc6dddddb37abaf5ab290673a13f7bb9a24f
kokspro <koks.programming@gmail.com>
2022-12-08 12:42:15 -0600

    New color theme and scroll animations added

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 let intro = document.querySelector('.intro');
 
-
 window.onload = init();
 
 function init(){
     setTimeout(function(){
         intro.remove()
-    }, 15000);
+    }, 8000);
 }
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+    })
+    },
+    {
+        threshold: 0.5,
+    }
+);
+
+hiddenElements.forEach(hiddenElements => {
+    observer.observe(hiddenElements);
+});
+
 
 
